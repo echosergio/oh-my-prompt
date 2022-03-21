a3a3b42332eb79a73f80db95b8b72761c3df1148
Sergio Vicente de las Heras <s.vicente@peza.io>
2022-03-21 20:00:56 +0100

    Minor fix

--- Install-OhMyPosh.ps1
+++ Install-OhMyPosh.ps1
@@ -3,10 +3,6 @@ Param()
 
 Install-WinGetApp -App "JanDeDobbeleer.OhMyPosh" -ErrorAction Stop
 
-if (Test-Path $env:POSH_THEMES_PATH) {
-    Copy-Item "$PSScriptRoot\oh-my-posh\themes\custom.omp.json" -Destination $env:POSH_THEMES_PATH -Force
-}
-
 $themes = Get-ChildItem -Path "$PSScriptRoot\oh-my-posh\themes" -Recurse "*.json"
 foreach ($theme in $themes) {
     Copy-Item $theme.FullName -Destination $env:POSH_THEMES_PATH -Force
